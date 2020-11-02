7ce39d522f99a917a7dd41ef54e14324f7fbdb5e
Hamilton Resende <hamilton.rferreira@gmail.com>
2020-11-02 20:57:51 -0300

    Provocando erro ao testar o metodo de verificacao de dependencias ciclicas.

--- maventestproject/src/main/java/br/edu/maventestproject/persistence/FooDAO.java
+++ maventestproject/src/main/java/br/edu/maventestproject/persistence/FooDAO.java
@@ -1,5 +1,7 @@
 package br.edu.maventestproject.persistence;
 
+import br.edu.maventestproject.service.FooService;
+
 public class FooDAO implements DAO{
-    
+    FooService fooService = new FooService();
 }
