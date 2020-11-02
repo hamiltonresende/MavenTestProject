17a3d2916b6706fc7ed72705377e2e8ed657b4fe
Hamilton Resende <hamilton.rferreira@gmail.com>
2020-11-02 19:41:29 -0300

    Criando o projeto Maven.

---
+++ maventestproject/src/main/java/br/edu/maventestproject/App.java
@@ -0,0 +1,13 @@
+package br.edu.maventestproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
