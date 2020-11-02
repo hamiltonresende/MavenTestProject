b2356cd19fcdfd5880435649fe56a3ae03eb5276
Hamilton Resende <hamilton.rferreira@gmail.com>
2020-11-02 20:02:48 -0300

    Definindo as classes do projeto que serão testadas.

--- maventestproject/src/test/java/br/edu/maventestproject/FooArchitectTest.java
+++ maventestproject/src/test/java/br/edu/maventestproject/FooArchitectTest.java
@@ -2,14 +2,20 @@ package br.edu.maventestproject;
 
 import static org.junit.Assert.assertTrue;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+
 import org.junit.Test;
 
 public class FooArchitectTest {
+    // Define os pacotes sujeitos a realizacao dos testes
+    JavaClasses importedClasses = new ClassFileImporter().importPackages("br.edu.maventestproject");
     
     // Define que é um metodo de teste, logo significa que é uma classe que pode rodar testes
     @Test
-    public void verificarArquitetura(){
-        assertTrue(true);
+    public void verificarDependenciaParaCamadaPersistencia(){
+        
     }
 
 }
