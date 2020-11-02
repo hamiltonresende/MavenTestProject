842df3ee93ec64d3660ca7e6e8af1d3a28d6b79f
Hamilton Resende <hamilton.rferreira@gmail.com>
2020-11-02 20:11:09 -0300

    Criando regra para verificar dependencias para a camada de persistencia.

--- maventestproject/src/test/java/br/edu/maventestproject/FooArchitectTest.java
+++ maventestproject/src/test/java/br/edu/maventestproject/FooArchitectTest.java
@@ -1,12 +1,13 @@
 package br.edu.maventestproject;
 
-import static org.junit.Assert.assertTrue;
-
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
 
 import org.junit.Test;
+
+// A classe estatica classes deve ser importada manualmente
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 public class FooArchitectTest {
     // Define os pacotes sujeitos a realizacao dos testes
@@ -15,7 +16,15 @@ public class FooArchitectTest {
     // Define que é um metodo de teste, logo significa que é uma classe que pode rodar testes
     @Test
     public void verificarDependenciaParaCamadaPersistencia(){
-        
+        /* ArchRule serve para definir as regras */
+
+        /* Para as classes que estao no pacote persistencia, devem apenas ter dependencias 
+           que residem nos pacotes persistencia e servicos */
+        ArchRule rule = classes().that().resideInAPackage("..persistence..")
+        .should().onlyHaveDependentClassesThat().resideInAnyPackage("..persistence..", "..service..");
+
+        // Faz o check nas classes selecionadas do projeto de acordo com a regra criada
+        rule.check(importedClasses);
     }
 
 }
